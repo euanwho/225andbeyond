82f08b3abde7a446094be13ade12a87a311f00b6
Euan Williams <fromeuan@gmail.com>
2020-05-18 13:23:07 +0100

    chagne

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = '225andbeyond.com'
+SITEURL = 'https://225andbeyond.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
